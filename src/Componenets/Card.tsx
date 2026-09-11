8b99cf607842029d988e68e6d472a35abc043f20
Pranto779 <pranto77268@gmail.com>
2026-09-11 18:31:53 +0600

    Add Toastify

--- src/Componenets/Card.tsx
+++ src/Componenets/Card.tsx
@@ -1,19 +1,44 @@
 import { useState } from 'react';
 import type { IDataType } from './Type/IType';
+import { toast } from 'react-toastify';
 
 export interface CardProps {
   card: IDataType;
 }
 const Card = ({ card }: CardProps) => {
  const [isSelected,setisSelected]=useState(false)
- const HandleButton=()=>{
-setisSelected(!isSelected)
+const HandleButton = () => {
+  setisSelected(!isSelected);
+  if (!isSelected) {
+    toast.success('Added to Stock !', {
+position: "bottom-right",
+autoClose: 5000,
+hideProgressBar: false,
+closeOnClick: false,
+pauseOnHover: true,
+draggable: true,
+progress: undefined,
+theme: "light",
+});
+  } else {
+    toast.info('Remove To Stock!', {
+position: "bottom-right",
+autoClose: 5000,
+hideProgressBar: false,
+closeOnClick: false,
+pauseOnHover: true,
+draggable: true,
+progress: undefined,
+theme: "light",
 
- }
+});;
+  }
+};
+
 
   return (
     <div>
-   <div className=" w-sm rounded-lg shadow-lg  p-6 bg-white">
+   <div className=" rounded-2xl shadow-lg  p-6 bg-white border border-gray-300">
      
       <div className="flex items-center justify-between mb-4">
         <img src={card.icon} alt={card.alt} className="w-12 h-12" />
@@ -29,7 +54,7 @@ setisSelected(!isSelected)
       <p className="text-gray-600 mb-4">{card.description}</p>
 
     
-      <div className="flex flex-wrap justify-between gap-5 mb-4">
+      <div className="flex flex-wrap justify-between gap-5 mb-4 px-4">
         <span className="bg-gray-200 text-gray-700 text-xs px-2 py-1 rounded">
           {card.category}
         </span>
@@ -46,8 +71,8 @@ setisSelected(!isSelected)
    
 
   
-      <button onClick={HandleButton} className="w-full bg-gray-900 text-white py-2 rounded hover:bg-gray-800">
-       {isSelected?"Selected":"Add To Card"}
+      <button onClick={HandleButton} className={`w-full ${isSelected?"bg-pink-200 text-pink-700": "bg-gray-900 text-white" } py-2 rounded`} disabled={isSelected?true:false}>
+       {isSelected?"✔ Added To Stock":" Added To Stock"}
       </button>
     </div>
     
--- src/Componenets/PCard.tsx
+++ src/Componenets/PCard.tsx
@@ -9,12 +9,19 @@ const PCard = ({ pcard }: CardProps) => {
     console.log(PCard);
     
     return (
-        <div className="grid grid-cols-1 px-7 md:px-0 mx-auto md:mx-0 md:grid-cols-3 gap-4  mt-5 ">
+       <div className="flex justify-between gap-10 ">
 
+         <div className="grid grid-cols-1 px-7 flex-4  md:px-0 mx-auto md:mx-0 md:grid-cols-3 gap-4  mt-5 ">
             {
                 pcard.map(card=><Card card={card} key={card.id}></Card>)
             }
+           
         </div>
+         <div className="w-[200px] h-[40px] bg-amber-400 flex-1">
+nljbkhvjcgxfgchvjbknlm;
+            </div>
+       </div>
+       
     )
 }
 
