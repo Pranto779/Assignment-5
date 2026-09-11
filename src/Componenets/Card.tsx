0dcd7a64877b92b4b9399a18c63b38b7b117224a
Pranto779 <pranto77268@gmail.com>
2026-09-11 23:40:47 +0600

    add selected window and functional all kwy

--- src/Componenets/Card.tsx
+++ src/Componenets/Card.tsx
@@ -4,88 +4,80 @@ import { toast } from 'react-toastify';
 
 export interface CardProps {
   card: IDataType;
-  select:IDataType[]
-  setSelect:Dispatch<SetStateAction<IDataType[]>>
-  selectcard:IDataType
+  select: IDataType[];
+  setSelect: Dispatch<SetStateAction<IDataType[]>>;
 }
-const Card = ({ card , select,setSelect,selectcard}: CardProps) => {
- const [isSelected,setisSelected]=useState(false)
-const HandleButton = () => {
-  setisSelected(!isSelected);
-  setSelect([...select,card])
-  if (!isSelected) {
-    toast.success('Added to Stock !', {
-position: "bottom-right",
-autoClose: 5000,
-hideProgressBar: false,
-closeOnClick: false,
-pauseOnHover: true,
-draggable: true,
-progress: undefined,
-theme: "light",
-});
-  } else {
-    toast.info('Remove To Stock!', {
-position: "bottom-right",
-autoClose: 5000,
-hideProgressBar: false,
-closeOnClick: false,
-pauseOnHover: true,
-draggable: true,
-progress: undefined,
-theme: "light",
 
-});;
-  }
-};
-
+const Card = ({ card, select, setSelect }: CardProps) => {
+  const [isSelected, setisSelected] = useState(false);
+  const HandleButton = () => {
+    setisSelected(!isSelected);
+    setSelect([...select, card]);
+    if (!isSelected) {
+      toast.success('Added to Stock !', {
+        position: 'bottom-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+      });
+    } else {
+      toast.info('Remove To Stock!', {
+        position: 'bottom-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+      });
+    }
+  };
 
   return (
-    
-    <div className='flex justify-between gap-10'>
-   <div className=" rounded-2xl shadow-lg  p-6 bg-white border border-gray-300">
-     
-      <div className="flex items-center justify-between mb-4">
-        <img src={card.icon} alt={card.alt} className="w-12 h-12" />
-        <span className=  {`font-bold ${card.hipe==="Popular"?"text-sky-400":card.hipe=="Standard"?"text-green-400":card.hipe==="Fast"?"text-orange-400":card.hipe==="Top SQL"?"text-blue-500":"text-red-500"} text-xs px-3 py-1 rounded`}>
-          {card.hipe}
-        </span>
+    <div className="flex justify-between gap-10">
+      <div
+        className={` rounded-2xl shadow-lg  p-6 bg-white border ${isSelected ? 'border-red-500' : 'border-gray-300'} border-gray-300`}
+      >
+        <div className="flex items-center justify-between mb-4">
+          <img src={card.icon} alt={card.alt} className="w-12 h-12" />
+          <span
+            className={`font-bold ${card.hipe === 'Popular' ? 'text-sky-400' : card.hipe == 'Standard' ? 'text-green-400' : card.hipe === 'Fast' ? 'text-orange-400' : card.hipe === 'Top SQL' ? 'text-blue-500' : 'text-red-500'} text-xs px-3 py-1 rounded`}
+          >
+            {card.hipe}
+          </span>
+        </div>
+
+        <h2 className="text-2xl font-bold mb-2">{card.name}</h2>
+
+        <p className="text-gray-600 mb-4">{card.description}</p>
+
+        <div className="flex flex-wrap justify-between gap-5 mb-4">
+          <span className="bg-gray-200 text-gray-700 text-xs px-2 py-1 rounded">
+            {card.category}
+          </span>
+          <span className="bg-gray-200 text-gray-700 text-xs px-2 py-1 rounded">
+            {card.level}
+          </span>
+          <div className="flex items-center mb-4">
+            <span className="text-yellow-500">⭐</span>
+            <span className="ml-1 text-sm font-semibold">4.9</span>
+          </div>
+        </div>
+
+        <button
+          onClick={HandleButton}
+          className={`w-full ${isSelected ? 'bg-pink-200 text-pink-700' : 'bg-gray-900 text-white'} py-2 rounded`}
+          disabled={isSelected ? true : false}
+        >
+          {isSelected ? '✔Added To Stack' : ' Add To Stack'}
+        </button>
       </div>
-
-  
-      <h2 className="text-2xl font-bold mb-2">{card.name}</h2>
-
-      
-      <p className="text-gray-600 mb-4">{card.description}</p>
-
-    
-      <div className="flex flex-wrap justify-between gap-5 mb-4 px-4">
-        <span className="bg-gray-200 text-gray-700 text-xs px-2 py-1 rounded">
-          {card.category}
-        </span>
-        <span className="bg-gray-200 text-gray-700 text-xs px-2 py-1 rounded">
-          {card.level}
-        </span>
-           <div className="flex items-center mb-4">
-        <span className="text-yellow-500">⭐</span>
-        <span className="ml-1 text-sm font-semibold">4.9</span>
-      </div>
-      </div>
-
-   
-   
-
-  
-      <button onClick={HandleButton} className={`w-full ${isSelected?"bg-pink-200 text-pink-700": "bg-gray-900 text-white" } py-2 rounded`} disabled={isSelected?true:false}>
-       {isSelected?"✔ Added To Stock":" Added To Stock"}
-      </button>
     </div>
-    
-
- </div>
-   
-   
-   
   );
 };
 
--- src/Componenets/PCard.tsx
+++ src/Componenets/PCard.tsx
@@ -1,43 +1,66 @@
-
-import { useState } from "react";
-import Card from "./Card";
-import type { IDataType } from "./Type/IType";
+import { useState } from 'react';
+import Card from './Card';
+import type { IDataType } from './Type/IType';
+import { RxCross2 } from 'react-icons/rx';
 
 export interface CardProps {
-    pcard: IDataType[]
+  pcard: IDataType[];
 }
 
 const PCard = ({ pcard }: CardProps) => {
-     const [select, setSelect] = useState<IDataType[]>([]);
- 
-    
-    return (
-       <div className="flex justify-between gap-10 ">
+  const [select, setSelect] = useState<IDataType[]>([]);
 
-         <div className="grid grid-cols-1 px-7 flex-4  md:px-0 mx-auto md:mx-0 md:grid-cols-3 gap-4  mt-5 ">
-            {
-                pcard.map(card=><Card card={card} key={card.id} select={select} setSelect={setSelect}></Card>)
-            }
-           
+  return (
+    <div className="flex justify-between gap-10 ">
+      <div className="grid grid-cols-1 px-7 flex-4  md:px-0 mx-auto md:mx-0 md:grid-cols-3 gap-4  mt-5 ">
+        {pcard.map((card) => (
+          <Card
+            card={card}
+            key={card.id}
+            select={select}
+            setSelect={setSelect}
+          />
+        ))}
+      </div>
+      <div className=" hidden md:block mt-6 w-[220px] text-center">
+        <h2 className="text-2xl font-bold">Your Stack</h2>
+        <p className="text-gray-400">{select.length===0?" ":select.length} {select.length===0?"No Card In stack":"Technology Selected"}</p>
+        {select.map((selectcard) => (
+          <div className="flex flex-col gap-5 " key={selectcard.id}>
+            <div className="flex gap-4 items-center mt-5">
+              <img
+                src={selectcard.icon}
+                className="w-[50px] h-[50px]"
+                alt={selectcard.alt}
+              />
+              <div>
+                <p className="text-xl font-bold">{selectcard.name}</p>
+                <p className="text-gray-400 text-[12px]">
+                  {selectcard.category}
+                </p>
+              </div>
+              <RxCross2
+                className="text-red-500 cursor-pointer text-2xl"
+                onClick={() =>
+                  setSelect(select.filter((card) => card.id !== selectcard.id))
+                }
+              />
+            </div>
+          </div>
+        ))}
+
+        <div className="mx-auto mt-10 px-2">
+          <button
+            className="btn px-15 border border-orange-500 text-orange-600"
+            onClick={() => setSelect([])}
+          >
+            {' '}
+            Remove All
+          </button>
         </div>
-           <div className=" hidden md:block mt-6 w-[220px]">
-              <h2 className="text-2xl font-bold">Your Stack</h2>
-              <p className="text-gray-400">{select.length} Technology Selected</p>
-        {
-            select.map(selectcard=>(
-                <div className="flex gap-2 items-center mt-5">
-                    <img src={selectcard.icon} className="w-[50px] h-[50px]" alt={selectcard.alt} />
-                  <div>
-                    <p className="text-xl font-bold">{selectcard.name} </p>
-                    <p className="text-gray-400 text-[12px]">{selectcard.category} </p>
-                  </div>
-                </div>
-            ))
-        }
+      </div>
     </div>
-       </div>
-       
-    )
-}
+  );
+};
 
 export default PCard;
